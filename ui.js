b72a80dd9603efe74f8841c389e61ef959616b23
snavy27 <35447564+snavy27@users.noreply.github.com>
2019-05-26 19:32:28 +0530

    Add files via upload

---
+++ ui.js
@@ -0,0 +1,26 @@
+
+$(document).ready(function(){
+    $("#create").click(function(){
+
+localStorage.setItem('name' , document.getElementById('name').value);
+localStorage.setItem('email' , document.getElementById('email').value);
+localStorage.setItem('number' , document.getElementById('number').value);
+localStorage.setItem('profile' , document.getElementById('profile').value);
+localStorage.setItem('school' , document.getElementById('school').value);
+localStorage.setItem('institute' , document.getElementById('institute').value);
+localStorage.setItem('title1' , document.getElementById('title1').value);
+localStorage.setItem('project1' , document.getElementById('project1').value);
+localStorage.setItem('title2' , document.getElementById('title2').value);
+localStorage.setItem('project2' , document.getElementById('project2').value);
+localStorage.setItem('template' , num);  
+
+    })
+
+})
+
+
+
+
+
+
+
